ee7ac35ff2e2c3cf65539b7e441f8793f363da50
Rahul Kundu <121861377+rahulkundu1209@users.noreply.github.com>
2024-11-26 21:22:27 +0530

    navbar added

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import NavbarBackground from './NavbarBackground';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
     <>
-      <nav>
-        Navbar
-      </nav>
+      <NavbarBackground />
+      <div
+        class="flex justify-between ml-10 mr-10 mt-5"
+      >
+        <div>
+          <img src={logo} width="150px" height="auto" />
+        </div>
+        <div>
+          <button class="font-bold text-2xl text-[#E98074] hover:text-[#E85A4F]">
+            ALL COURSES
+          </button>
+        </div>
+        <div>
+          <button
+            class="
+            bg-[#D8C3A5] 
+            p-2 
+            rounded-md 
+            text-xl 
+            font-semibold 
+            hover:bg-[#E98074]
+            hover:text-[#FFFFFF]"
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
     </>
   )
 }
---
+++ frontend/src/components/NavbarBackground.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavbarBackground = () => {
+  return (
+    <div
+      class="absolute -z-10 top-0 left-0 w-screen"
+    >
+      <svg
+      class="block"
+      width="100%" height="auto" viewBox="0 0 1366 359" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1366 0V48.5C1366 48.5 1315 128.5 1226.5 165C1064.47 231.827 796.184 118.579 597.5 145.5C335.5 181 147.5 365.5 155 358C162.5 350.5 323.5 140 349.5 111C375.5 82 316 125 174 145.5C75.5 159.72 0 130 0 130V0H1366Z" fill="#8E8D89" />
+      </svg>
+    </div>
+  )
+}
+
+export default NavbarBackground;
